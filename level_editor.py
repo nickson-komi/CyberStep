aaf93b371803bc7ced6d1e2e4553144ad44fe28c
NICKSON <nickson-komi@yandex.ru>
2025-05-03 03:16:32 +0300

    working on the level editor

--- level_editor.py
+++ level_editor.py
@@ -7,8 +7,8 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 class EditWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setObjectName("Редактор уровней")
         uic.loadUi('data/editor_menu.ui', self)
+        self.setFixedSize(self.size())
 
 
 if __name__ == '__main__':
